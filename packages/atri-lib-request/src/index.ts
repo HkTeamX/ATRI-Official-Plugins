f408378a82e458e527a99e297dc530fa9c6c018d
huankong233 <huank0ng@foxmail.com>
2025-09-12 13:47:45 +0800

    [*] 修复自动重试

--- packages/atri-lib-request/src/index.ts
+++ packages/atri-lib-request/src/index.ts
@@ -1,15 +1,25 @@
-import axios, { type AxiosRequestConfig } from 'axios'
+import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios'
+import axiosRetry from 'axios-retry'
 
 export class Axios {
-  static axiosInstance = axios.create({
-    timeout: 300000,
-  })
+  static axiosInstance: AxiosInstance
+
+  static initAxiosInstance() {
+    if (!Axios.axiosInstance) {
+      Axios.axiosInstance = axios.create({
+        timeout: 300000,
+      })
+      axiosRetry(Axios.axiosInstance, { retries: 3 })
+    }
+  }
 
   static get<T, P = null>(url: string, config?: AxiosRequestConfig<P>) {
+    Axios.initAxiosInstance()
     return Axios.axiosInstance.get<T>(url, config)
   }
 
   static post<T, P = object, U = null>(url: string, data?: P, config?: AxiosRequestConfig<U>) {
+    Axios.initAxiosInstance()
     return Axios.axiosInstance.post<T>(url, data, config)
   }
 }
